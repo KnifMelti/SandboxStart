e13ef2a978a80fbb44ae3b94b8a4328102d9270f
KnifMelti <gaj@knifmelti.se>
2026-01-03 13:13:55 +0100

    Convert default scripts to UTF-8 encoding

--- Source/assets/scripts/Std-WAU.ps1
+++
@@ -1,8 +0,0 @@
-$SandboxFolderName = "DefaultFolder"
-$InstallCmdPath = "$env:USERPROFILE\Desktop\$SandboxFolderName\InstallWSB.cmd"
-
-if (-not (Test-Path -Path $InstallCmdPath)) {
-    throw "InstallWSB.cmd not found at: $InstallCmdPath"
-}
-
-Start-Process cmd.exe -ArgumentList "/c del /Q `"$env:USERPROFILE\Desktop\$SandboxFolderName\*.log`" & `"$env:USERPROFILE\Desktop\$SandboxFolderName\InstallWSB.cmd`" && explorer `"$env:USERPROFILE\Desktop\$SandboxFolderName`""
