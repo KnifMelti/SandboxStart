32c43254dfcf387f588443bf51bbdafb28f59e1b
KnifMelti <gaj@knifmelti.se>
2026-01-05 18:52:49 +0100

    Remove temporary PowerShell script for uninstall completion message

--- Source/tempCodeRunnerFile.ps1
+++
@@ -1,6 +0,0 @@
-       [System.Windows.Forms.MessageBox]::Show(
-            $message,
-            "Uninstall Complete",
-            [System.Windows.Forms.MessageBoxButtons]::OK,
-            [System.Windows.Forms.MessageBoxIcon]::Information
-        )
